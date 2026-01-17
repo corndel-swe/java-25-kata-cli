6e3535ec83d077796291547b60df17d12cd2bde3
SoftwareEngineer101 <tanianazarian125@gmail.com>
2026-01-17 16:13:20 +0000

    PlayingCard is Complete

--- src/main/java/org/kata/tania/PlayingCard.java
+++ src/main/java/org/kata/tania/PlayingCard.java
@@ -52,9 +52,9 @@ public class PlayingCard implements Comparable<PlayingCard> {
     @Override
     public int compareTo(PlayingCard playingCard) {
         int suitComparison = this.suit.compareTo(playingCard.suit);
-        //If Order differs
+        //Returns -1,0,1 depending on condition
         if (suitComparison != 0) {return suitComparison;}
-        //If Order is the same
+
         return Integer.compare(this.value, playingCard.value);
 
 
